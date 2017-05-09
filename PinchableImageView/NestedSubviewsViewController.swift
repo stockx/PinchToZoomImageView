3cae0e0ad28df0f7f722140756a7490b4e329271
Josh Sklar <jrmsklar@gmail.com>
2017-05-09 14:49:58 -0400

    add other demo view controllers

---
+++ PinchableImageView/NestedSubviewsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NestedSubviewsViewController.swift
+//  PinchableImageView
+//
+//  Created by Josh Sklar on 5/9/17.
+//  Copyright © 2017 StockX. All rights reserved.
+//
+
+import UIKit
+
+class NestedSubviewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
